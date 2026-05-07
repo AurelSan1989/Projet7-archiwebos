591dbc756fcfb745fa231991ba29d38c1f1de890
AurelSan1989 <aurelien.girodet@gmail.com>
2026-05-07 17:00:11 +0200

    Création de la page login.html & correction de login.js : On arrête d'intégrer le formulaire de connexion par JS, mais on le fait par HTML. Le fichier JS gère seulement la connection et stocke le token de l'utilisateur dans le localStorage

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,78 +1,10 @@
-const logInBtn = document.getElementById('logIn');
-logInBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    const mainElement = document.querySelector('main');
-    mainElement.innerHTML = "" //On vide la balise <main>
+// 
 
-    //On crée une section avec l'id contact
-    const logInSection = document.createElement('section');
-    logInSection.id = "login";
-    mainElement.appendChild(logInSection);
+const formElement = document.querySelector('.login-form');
+const emailInput = document.getElementById('email');
+const passWordInput = document.getElementById('password');
 
-    //Création de la balise titre (h2)
-    const pageTitle = document.createElement('h2');
-    pageTitle.classList.add('login-title');
-    pageTitle.textContent = 'Log in';
-    logInSection.appendChild(pageTitle);
-
-    //Création de la balise form
-    const formElement = document.createElement('form');
-    formElement.classList.add('login-form');
-    logInSection.appendChild(formElement);
-
-    //Création d'une balise fieldset pour regrouper un label et son input
-    const emailGroup = document.createElement('fieldset');
-    emailGroup.classList.add('form-group');
-
-    //Création du label pour l'email
-    const emailLabel = document.createElement('label');
-    emailLabel.setAttribute('for', 'email');
-    emailLabel.textContent = 'E-mail';
-    emailGroup.appendChild(emailLabel); // On insère dans le fieldset 
-
-    //Création de l'input pour l'email
-    const emailInput = document.createElement('input');
-    emailInput.type = 'email';
-    emailInput.id = 'email';
-    emailInput.name = 'email';
-    emailGroup.appendChild(emailInput); // On insère dans le fieldset
-
-    formElement.appendChild(emailGroup); // On insère le fieldset dans la balise form
-
-    //Création d'une balise fieldset
-    const passWordGroup = document.createElement('fieldset');
-    passWordGroup.classList.add('form-group');
-
-    //Création du label pour le mot de passe
-    const passWordLabel = document.createElement('label');
-    passWordLabel.setAttribute('for', 'password');
-    passWordLabel.textContent = 'Mot de passe';
-    passWordGroup.appendChild(passWordLabel); // On insère dans le fieldset
-
-    //Création de l'input pour le mot de passe
-    const passWordInput = document.createElement('input');
-    passWordInput.type = 'password';
-    passWordInput.id = "password"
-    passWordInput.name = "password";
-    passWordGroup.appendChild(passWordInput); // On insère dans le fieldset
-
-    formElement.appendChild(passWordGroup); // On insère le fieldset dans la balise form
-
-    //Création du bouton "Se connecter"
-    const connectBtn = document.createElement('button');
-    connectBtn.type = 'submit';
-    connectBtn.classList.add('connect-btn');
-    connectBtn.textContent = "Se connecter";
-    formElement.appendChild(connectBtn); // On insère le boutton à la fin du formulaire
-
-    //Création du lien "Mot de passe oublié"
-    const formFooter = document.createElement('a');
-    formFooter.classList.add('form-footer');
-    formFooter.href = '#';
-    formFooter.textContent = 'Mot de passe oublié';
-    logInSection.appendChild(formFooter); // Qu'on insère après le formulaire
-
-    formElement.addEventListener('submit', function(event) {
+formElement.addEventListener('submit', function(event) {
     event.preventDefault();
 
     // On récupère les valeurs saisies par l'utilisateur
@@ -80,7 +12,7 @@ logInBtn.addEventListener('click', function(event) {
     const userPassword = passWordInput.value;
     
     // On prépare les données à envoyer dans le corps de la requête
-    const data = {
+    const userData = {
         email: userEmail,
         password : userPassword
     }
@@ -90,7 +22,7 @@ logInBtn.addEventListener('click', function(event) {
         headers : {
             'Content-Type' : 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(userData)
     })
     .then (response => {
         if(!response.ok) {
@@ -116,5 +48,3 @@ logInBtn.addEventListener('click', function(event) {
         logInSection.appendChild(errorMessage)
     })
 })
-})
-
